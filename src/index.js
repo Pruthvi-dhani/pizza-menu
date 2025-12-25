3f1f81b4a6544594f3ff41ce52623c9255729c2e
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2025-12-25 20:39:43 +0000

    adds all the code until before coding challenge 1

--- src/index.js
+++ src/index.js
@@ -70,19 +70,32 @@ function Menu() {
   return (
     <main className="menu">
       <h2> Our Menu </h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Salamino"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, Funghi"
+        photoName="pizzas/funghi.jpg"
+        price={10}
+      />
     </main>
   );
 }
 
-function Pizza() {
+function Pizza(props) {
+  console.log(props);
   return (
-    <div>
-      <h3>Pizza Salamino</h3>
-      <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinnach" />
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 4}</span>
+      </div>
     </div>
   );
 }
