2ab533519247e8b44c4265420c4e7058becbd5fc
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2025-12-26 14:26:59 +0000

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -67,14 +67,20 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2> Our Menu </h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our Menu, please come back later.</p>
+      )}
     </main>
   );
 }
@@ -107,7 +113,20 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open!
+      {isOpen ? (
+        <div className="order">
+          <p>
+            We're open until {`${closedHour}:00`}. Come visit us or order
+            online.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      ) : (
+        <p>
+          Currently closed, but we're happy to welcome you between{" "}
+          {`${openHour}:00`} and {`${closedHour}:00`}
+        </p>
+      )}
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
